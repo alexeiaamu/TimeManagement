5ae778fb35f62b3691f3963ebe85a7818aa644c4
AlexeiAamu <alexeiaamu@gmail.com>
2024-11-05 14:15:52 +0200

    updates on the code

--- timelogger.py
+++ timelogger.py
@@ -1,5 +1,22 @@
-import psycopg2
-from psycopg2.extras import RealDictCursor
-from config import config
-import json
+from flask import Flask, request
+from functions import db_create_log
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return {"index": True}
+
+@app.route("/timelog", methods=['POST'])
+def create_log():
+    try: 
+        data = request.get_json()
+        start_time = data['start_time']
+        end_time = data['end_time']
+        lunch_break = data['lunch_break']
+        consultant_name=data['consultant_name']
+        customer_name=data['customer_name']
+        db_create_log(start_time, end_time, lunch_break, consultant_name, customer_name)
+        return {"Time logged"}
+    except:
+        return {"error logging time, please check input and try again"}
